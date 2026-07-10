966a292d94557b481b4044a87a0e36af2051453a
dkmh2122220002 <dkmh2122220002@gmail.com>
2026-07-10 17:33:14 +0700

    Add new NSG to allow web traffic

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_version = "~>1.15.0"
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "=3.112.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.112.0"
     }
   }
   cloud {
-    
+
     organization = "dkmh2122220002"
 
     workspaces {
@@ -18,71 +18,91 @@ terraform {
 }
 provider "azurerm" {
   features {
-    
+
   }
   #resource_provider_registrations = false
   skip_provider_registration = true
 }
 resource "azurerm_resource_group" "rg" {
-  name = "1-0a0e4573-playground-sandbox"
+  name     = "1-0a0e4573-playground-sandbox"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "VNET01" {
-  name = "VNET01"
-  location = azurerm_resource_group.rg.location
+  name                = "VNET01"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "VNET01_SUBNET1" {
-  name = "VNET01_SUBNET1"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "VNET01_SUBNET1"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.VNET01.name
-  address_prefixes = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "VNET01_SUBNET2" {
-  name = "VNET01_SUBNET2"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "VNET01_SUBNET2"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.VNET01.name
-  address_prefixes = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 
 resource "azurerm_network_interface" "name" {
-  name = "VM01_NIC"
+  name                = "VM01_NIC"
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
   ip_configuration {
-    name = "VM01_NIC_IP01"
-    subnet_id = azurerm_subnet.VNET01_SUBNET1.id
+    name                          = "VM01_NIC_IP01"
+    subnet_id                     = azurerm_subnet.VNET01_SUBNET1.id
     private_ip_address_allocation = "Static"
-    private_ip_address = "10.0.1.100"
+    private_ip_address            = "10.0.1.100"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "VM01" {
-  name = "VM01"
-  resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  size = "Standard_D2s_v3"
-  admin_username = "hien"
-  admin_password = "xinchao1A"
+  name                            = "VM01"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "hien"
+  admin_password                  = "xinchao1A"
   disable_password_authentication = "false"
-  network_interface_ids = [azurerm_network_interface.name.id]
+  network_interface_ids           = [azurerm_network_interface.name.id]
   os_disk {
-    caching = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
   source_image_reference {
-  publisher = "canonical"
-  offer     = "ubuntu-24_04-lts"
-  sku       = "ubuntu-pro-gen1"
-  version   = "latest"
+    publisher = "canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "ubuntu-pro-gen1"
+    version   = "latest"
   }
   tags = {
     ManagedBy = "Hien"
   }
 }
 
+resource "azurerm_network_security_group" "NSG_Allow_Web" {
+  name                = "NSG_Allow_Web"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rule {
+    name                       = "Allow_HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  tags = {
+    Type      = "NSG"
+    ManagedBy = "Terraform"
+  }
+}
